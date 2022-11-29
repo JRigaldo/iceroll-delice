81a9a61c0c19c5a13d4a451c8aa9ea944657f9f4
Jeremy <rigaldo.jeremy@gmail.com>
2022-11-29 22:46:39 +0100

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,40 @@
+var swiperL = document.querySelector('.swiper-l');
+if (typeof swiperL !== 'undefined' && swiperL) {
+    var swiperLeft = new Swiper(".swiper-l", {
+        slidesPerView: 1,
+        centeredSlides: false,
+        spaceBetween: 35,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+            type: 'bullets',
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            500: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+                mode: 'horizontal',
+                freeMode: true,
+            },
+            768: {
+
+                slidesPerView: 3,
+                spaceBetween: 35,
+            },
+            1100: {
+                slidesPerView: 4,
+
+            },
+            1920: {
+                slidesPerView: 7,
+                spaceBetweenSlides: 35
+            }
+        }
+    });
+}
