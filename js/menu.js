c1419168e9b77b68d500c52d18a7a8658efa91eb
Jeremy <rigaldo.jeremy@gmail.com>
2022-12-07 09:50:42 +0100

    add navabar N mobile nav

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,9 @@
 jQuery(function ($) {
     $(document).ready(function () {
         function responsive() {
-            $("#header-icon-social").click(function (e) {
-                e.preventDefault()
-                $("body").addClass("with-sidebar-social")
-                if ($("body").has('with-sidebar-links')) {
-                    $("body").removeClass("with-sidebar-links")
-                }
-            })
-            $("#exit-menu-social").click(function (e) {
-                e.preventDefault()
-                $("body").removeClass("with-sidebar-social")
-            })
             $("#header-icon-links").click(function (e) {
                 e.preventDefault()
                 $("body").addClass("with-sidebar-links")
-                if ($("body").has('with-sidebar-social')) {
-                    $("body").removeClass("with-sidebar-social")
-                }
             })
             $("#exit-menu-links").click(function (e) {
                 e.preventDefault()
@@ -26,7 +12,7 @@ jQuery(function ($) {
 
             $("#site-cover").click(function (e) {
                 e.preventDefault()
-                $("body").removeClass("with-sidebar-social with-sidebar-links")
+                $("body").removeClass("with-sidebar-links")
             })
         }
         responsive()
