9c98d2f9daccbff98a841d53a789ac11c00285d6
Jeremy <rigaldo.jeremy@gmail.com>
2022-12-11 00:02:09 +0100

    add form event page

--- js/menu.js
+++ js/menu.js
@@ -4,15 +4,17 @@ jQuery(function ($) {
             $("#header-icon-links").click(function (e) {
                 e.preventDefault()
                 $("body").addClass("with-sidebar-links")
+                $("html").css('overflow-y', 'hidden');
             })
             $("#exit-menu-links").click(function (e) {
                 e.preventDefault()
                 $("body").removeClass("with-sidebar-links")
+                $("html").css('overflow-y', 'scroll');
             })
-
             $("#site-cover").click(function (e) {
                 e.preventDefault()
                 $("body").removeClass("with-sidebar-links")
+                $("html").css('overflow-y', 'scroll');
             })
         }
         responsive()
