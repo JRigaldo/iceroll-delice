12e8468624bc34cbe06ef4d629d884b277129a13
Jeremy <rigaldo.jeremy@gmail.com>
2022-12-14 23:49:08 +0100

    add transition animation on form-modal

--- js/modal.js
+++ js/modal.js
@@ -1,52 +1,90 @@
 jQuery(function ($) {
     $(document).ready(function () {
-        $('#next1').click(function () {
-            addAnimation('#modalBox1', 'fadeOut');
-            addAnimation('#modalBox2', 'fadeIn');
+
+        /*
+        $('.next-1').click(function () {
+            addAnimation('.step-1', 'fadeOut');
+            addAnimation('.step-2', 'fadeIn');
+        });
+
+        $('.next-2').click(function () {
+            addAnimation('.step-2', 'fadeOut');
+            addAnimation('.step-3', 'fadeIn');
+        });
+
+        $('.prev-2').click(function () {
+            addAnimation('.step-2', 'fadeIn');
+            addAnimation('.step-1', 'fadeOut');
+        });
+
+        $('.prev-3').click(function () {
+            addAnimation('.step-3', 'fadeOut');
+            addAnimation('.step-2', 'fadeIn');
         });
 
         function addAnimation(element, animation) {
             if (animation === 'fadeIn') {
                 $(element).removeClass('animated-disable');
             }
-            removeAnimation(element, animation);
-            $(element).addClass('animated ' + animation);
-        }
-
-        function removeAnimation(element, animation) {
-            $(element).removeClass('animated ' + animation);
             if (animation === 'fadeOut') {
                 $(element).addClass('animated-disable');
             }
-
+            $(element).removeClass(animation);
+            $(element).addClass(animation);
         }
 
+
+         */
+
+
+        $('.next-1').click(function () {
+            fadeAnimation('.step-1', 'fadeOut', '.step-2', 'fadeIn');
+            activeTransition('.step-1', '.step-2');
+            scrollTop('.step-2');
+        });
+
+        $('.next-2').click(function (event) {
+            fadeAnimation('.step-2', 'fadeOut', '.step-3', 'fadeIn');
+            activeTransition('.step-2', '.step-3');
+            scrollTop('.step-3');
+        });
+
+        $('.prev-2').click(function (event) {
+            fadeAnimation('.step-2', 'fadeOut', '.step-1', 'fadeIn');
+            activeTransition('.step-2', '.step-1');
+            scrollTop('.step-1');
+        });
+
+        $('.prev-3').click(function () {
+            fadeAnimation('.step-3', 'fadeOut', '.step-2', 'fadeIn');
+            activeTransition('.step-3', '.step-2');
+            scrollTop('.step-2');
+        });
+
+        function fadeAnimation(prevElement, prevAnimation, nextElement, nextAnimation) {
+            $(nextElement).addClass(nextAnimation);
+            $(prevElement).addClass(prevAnimation);
+            if($(prevElement).hasClass('fadeIn') === true){
+                $(prevElement).removeClass('fadeIn');
+            }
+            if($(nextElement).hasClass('fadeOut') === true){
+                $(nextElement).removeClass('fadeOut');
+            }
+        }
+
+        function activeTransition(prevElement, nextElement){
+            $(nextElement).addClass('animated-active');
+            $(prevElement).removeClass('animated-active');
+            setTimeout(function(){
+                $(nextElement).removeClass('animated-disable');
+                $(prevElement).addClass('animated-disable');
+            }, 500);
+        }
+
+        function scrollTop(container){
+            $("html, body").animate({
+                scrollTop: $(container).offset().top - 100
+            }, 200);
+        }
     });
 });
-
-/*
-// Step 1
-$('#modalBox1').click(e => {
-    e.preventDefault();
-    $('#dialog-box-1').removeClass('dialog-box-1');
-    $('#dialog-box-2').addClass('dialog-box-2');
-    e.stopPropagation();
-});
-
-// Step 2
-$('#modalBox2').click(e => {
-    e.preventDefault();
-    $('#dialog-box-2').removeClass('dialog-box-2');
-    $('#dialog-box-3').addClass('dialog-box-3');
-    e.stopPropagation();
-});
-
-// Step 3
-$('#modalBox3').click(e => {
-    e.preventDefault();
-    $('.dialog').removeClass('dialog-open');
-    $('#dialog-box-3').removeClass('dialog-box-3');
-    e.stopPropagation();
-});
-
- */
